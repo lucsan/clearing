a98ff5362fca549dfcf7b66f70ba58a2f0a2e924
lucsan <Luke.Tennant@gmail.com>
2018-09-27 20:17:04 +0100

    Updated globals in actions.js.

--- app/components/actions.js
+++ app/components/actions.js
@@ -1,35 +1,35 @@
 /*
 global
-thingsHandler: true,
 character: true,
 places: true,
-things: true,
-log: false
+things: true
 */
 const actions = () => {
+  const stage = stage()
+  const thand = thingsHandler()
 
   const pickUp = (id) => {
-    thingsHandler().moveThingFromContainerToContainer(id, 'env', 'inv')
-    stage().respond(`You picked up the ${id} and put it in your pack.`)
+    thand.moveThingFromContainerToContainer(id, 'env', 'inv')
+    stage.respond(`You picked up the ${id} and put it in your pack.`)
     log(`picked up ${id}`)
   }
 
   const drop = (id) => {
-    thingsHandler().moveThingFromContainerToContainer(id, 'inv', 'env')
+    thand.moveThingFromContainerToContainer(id, 'inv', 'env')
     let place = places[character.location]
-    stage().respond(`You drop the ${id} in ${place.desc}.`)
+    stage.respond(`You drop the ${id} in ${place.desc}.`)
     log(`droped ${id}`)
   }
 
   const hold = (id) => {
-    thingsHandler().moveThingFromContainerToContainer(id, 'inv', 'bod')
-    stage().respond(`You grasp the ${id} in your hand.`)
+    thand.moveThingFromContainerToContainer(id, 'inv', 'bod')
+    stage.respond(`You grasp the ${id} in your hand.`)
     log(`${id} in hand`)
   }
 
   const bagit = (id) => {
-    thingsHandler().moveThingFromContainerToContainer(id, 'bod', 'inv')
-    stage().respond(`You put the ${id} in your pack.`)
+    thand.moveThingFromContainerToContainer(id, 'bod', 'inv')
+    stage.respond(`You put the ${id} in your pack.`)
     log(`${id} in pack`)
   }
 
@@ -47,7 +47,7 @@ const actions = () => {
     console.log('target def', targetObj.properties.defense, 'weapon atk', weaponObj.properties.attack);
 
     console.log(`You hit ${target} for ${dam} damage`);
-    stage().respond(`You hit ${things[target].desc} with ${weapon} for ${dam} damage`)
+    stage.respond(`You hit ${things[target].desc} with ${weapon} for ${dam} damage`)
     log(`You hit ${things[target].desc} with ${weapon} for ${dam} damage`)
 
   }
